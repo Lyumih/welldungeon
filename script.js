33fbdeb0818f9f1a71e07fa78db4ed6bf8b46156
Lyumih <m.ivanov1225@gmail.com>
2020-01-07 16:37:08 +0300

    Attack

---
+++ script.js
@@ -0,0 +1,26 @@
+/**Секунд для действия */
+let secondTimeout = 4
+/* Порядок действий */
+let buttonsWords = ['Исследовать уровень', 'Продолжить']
+
+// let keyboard = document.querySelector('.Keyboard__container')
+// let buttons = keyboard.getElementsByTagName('button')
+
+
+function run() {
+    console.log("Скрипт запущен")
+    let timerId = setInterval(() => {
+        let keyboard = document.querySelector('.Keyboard__container')
+        console.log("Работает скрипт!")
+        let buttons = keyboard.getElementsByTagName('button')
+        if (buttons) clickButton(buttons[0])
+        else console.log("Нечего нажимать")
+    }, secondTimeout * 1000)
+}
+
+function clickButton(element) {
+    console.log("Происходит клик по кнопке", element.innerText)
+    element.click()
+}
+
+run(secondTimeout, buttonsWords)
